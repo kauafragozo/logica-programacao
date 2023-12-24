7baf4bea8a55547fb8db0454091672deea32bffa
Kauã <kauafragozo18@gmail.com>
2023-12-24 17:27:34 -0300

    primeira commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool executando = true;
+            while (executando)
+            {
+                Console.WriteLine("Escolha o Primeiro Número");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Calculadora em C#");
+                Console.WriteLine("Escolha uma Operação:");
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Escolha o Segundo Número");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                int resultado = 0;
+
+                switch (op)
+                {
+                    case 1:
+                        resultado = n1 + n2;
+                        break;
+                    case 2:
+                        resultado = n1 - n2;
+                        break;
+                    case 3:
+                        resultado = n1 * n2;
+                        break;
+                    case 4:
+                        resultado = n1 / n2;
+                        break;
+                    default:
+                        Console.WriteLine("Operação Inválida");
+                        break;
+                }
+                Console.WriteLine("-------------------");
+                Console.WriteLine(resultado);
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Deseja Realizar outra operação?");
+                Console.WriteLine("1 - Sim");
+                Console.WriteLine("2- Não");
+                int resposta = Convert.ToInt32(Console.ReadLine());
+
+                switch (resposta)
+                {
+                    case 1:
+                        executando = true;
+                        break;
+                    case 2:
+                        executando = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Estoque/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldoinicial = 0;
+            bool executando = true;
+            while (executando)
+            {
+                Console.WriteLine("Insira o Saldo Incicial");
+                saldoinicial = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("----------");
+                Console.WriteLine("Selecione uma Opção");
+                Console.WriteLine("1 - Entrada");
+                Console.WriteLine("2 - Saida");
+                int tipo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Insira a Quantidade");
+                int quantidade = Convert.ToInt32(Console.ReadLine());
+
+                switch (tipo)
+                {
+                    case 1:
+                        saldoinicial += quantidade;
+                        Console.WriteLine("O saldo é igual a:" + saldoinicial);
+                        break;
+                    case 2:
+                        if (saldoinicial >= quantidade)
+                        {
+                            saldoinicial -= quantidade;
+                            Console.WriteLine("O saldo é igual a:" + saldoinicial);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Quantidade Insuficiente");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Operação Inválida");
+                        break;
+                }
+                Console.WriteLine("Deseja Realizar outra Operação?");
+                Console.WriteLine("1 - Sim");
+                Console.WriteLine("2 - Não");
+                int op2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (op2)
+                {
+                    case 1:
+                        executando = true;
+                        break;
+                    case 2:
+                        executando = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+            }
+        }
+    }
+}
